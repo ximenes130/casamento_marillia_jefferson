92afa5814820c78c26e026ec89ca573579c770be
ximenes <ximenes130@gmail.com>
2018-02-12 17:32:48 -0200

    Adding header

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	var nav = $('nav')
 	
+	$('.scrollspy').scrollSpy({scrollOffset: 70});
+	$('.parallax').parallax();
+
+	nav.pushpin({
+		top: nav.offset().top
+	});
+
 });
